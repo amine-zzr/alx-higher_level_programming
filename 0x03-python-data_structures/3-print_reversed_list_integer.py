f391b6a24e35cd9c182a70e4375e2b772dbda884
amine-zzr <aminezoukri@gmail.com>
2024-01-08 20:11:02 +0000

    printing list elements in reverese

---
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def print_reversed_list_integer(my_list=[]):
+
+    lenght = (len(my_list) - 1)
+    while lenght >= 0:
+            print("{:d}".format(my_list[lenght]))
+            lenght -= 1
