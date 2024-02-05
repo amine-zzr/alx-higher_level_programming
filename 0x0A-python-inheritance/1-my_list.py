b4993b655f089b9a0318e102c1ffc0b334bdcce3
amine-zzr <aminezoukri@gmail.com>
2024-02-05 21:00:30 +0000

    fixing code

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -12,5 +12,4 @@ class MyList(list):
         """
         Prints the list in sorted order (ascending).
         """
-        sorted_list = sorted(self)
-        print(sorted_list)
+        print(sorted(list(self)))
