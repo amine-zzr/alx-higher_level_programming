bd334248079726f620d77b819732c013a4be214f
amine-zzr <aminezoukri@gmail.com>
2024-02-05 18:57:50 +0000

    public method to print a list in ascending order

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""
+defines a class that has a public method to print a list in sorted oreder.
+"""
+
+
+class MyList(list):
+    """
+    MyList class inherits from the built-in list class.
+    """
+    def print_sorted(self):
+        """
+        Prints the list in sorted order (ascending).
+        """
+        sorted_list = sorted(self)
+        print(sorted_list)
