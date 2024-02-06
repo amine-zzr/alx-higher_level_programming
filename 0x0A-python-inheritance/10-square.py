fe24db1cfc3e981d859ca6ad66b0107d8a9945fe
amine-zzr <aminezoukri@gmail.com>
2024-02-06 16:52:21 +0000

    using the super() to call the constructor of the superclass Rectangle with size provided as both width and height

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+'''
+'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''
+    '''
+
+    def __init__(self, size):
+        '''
+        Initializes a Square object with size.
+
+        Parameters:
+        - size: Size of the square.
+
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is less than or equal to 0.
+        '''
+
+        super().__init__(size, size)
+        self.__size = size
+        self.integer_validator("size", size)
+
+    def __str__(self):
+        '''
+        Returns the string representation of the square.
+        '''
+
+        return "[Rectangle] {}/{}".format(self.__size, self.__size)
