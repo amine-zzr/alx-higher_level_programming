825f375e552adf70875b84f5db68d01825d7fdd2
amine-zzr <aminezoukri@gmail.com>
2024-02-06 17:05:14 +0000

    str() magic method for tha square class

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,35 @@
+#!/usr/bin/python3
+'''
+This Module intializes the Square class.
+'''
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    '''
+    Square class inherits from Rectangle.
+    '''
+
+    def __init__(self, size):
+        '''
+        Initializes a Square object with size.
+
+        Parameters:
+        - size: Size of the square.
+
+        Raises:
+        - TypeError: If size is not an integer.
+        - ValueError: If size is less than or equal to 0.
+        '''
+
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        '''
+        Returns the string representation of the square.
+        '''
+
+        return "[Square] {}/{}".format(self.__size, self.__size)
