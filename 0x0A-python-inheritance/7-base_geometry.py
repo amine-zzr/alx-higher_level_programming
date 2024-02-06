f9e42b4f05fdb5e82e97af2feeac46a937c89814
amine-zzr <aminezoukri@gmail.com>
2024-02-06 14:02:52 +0000

    TDD again

---
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""
+Module with a class BaseGeometry.
+"""
+
+
+class BaseGeometry:
+    """
+    BaseGeometry class provides a base for geometry-related classes.
+    """
+    def area(self):
+        """
+        Public instance method that raises an Exception with the message
+        'area() is not implemented'.
+        """
+        raise Exception("area() is not implemented")
+
+    def integer_validator(self, name, value):
+        """
+        Public instance method that validates the value to be
+        an integer greater than 0.
+        """
+        if not isinstance(value, int):
+            raise TypeError("{} must be an integer".format(name))
+
+        if value <= 0:
+            raise ValueError("{} must be greater than 0".format(name))
