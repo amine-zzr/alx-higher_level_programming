18b4462e7fdbf2bd6adaf712b8ee4740ebc49292
amine-zzr <aminezoukri@gmail.com>
2024-02-07 20:37:18 +0000

    using the vars() method to return the dict of an instance is a more concise way to achieve the same result without hardcoding all attributes individually

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -23,8 +23,4 @@ class Student:
         Retrieve a dictionary representation of a Student instance.
         '''
 
-        return {
-            'first_name': self.first_name,
-            'last_name': self.last_name,
-            'age': self.age
-        }
+        return vars(self)
