14dfd6c507fbb574228b167052fdfc282d10ceb2
amine-zzr <aminezoukri@gmail.com>
2024-02-11 14:14:49 +0000

    pushing the new changes on the file

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -30,6 +30,10 @@ class Rectangle(Base):
     @width.setter
     def width(self, value):
         """Setter method for width attribute."""
+        if not isinstance(value, int):
+            raise TypeError('width must be an integer')
+        elif value <= 0:
+            raise ValueError('width must be > 0')
         self.__width = value
 
     @property
@@ -40,6 +44,10 @@ class Rectangle(Base):
     @height.setter
     def height(self, value):
         """Setter method for height attribute."""
+        if not isinstance(value, int):
+            raise TypeError('height must be an integer')
+        elif value <= 0:
+            raise ValueError('height must be > 0')
         self.__height = value
 
     @property
@@ -50,6 +58,10 @@ class Rectangle(Base):
     @x.setter
     def x(self, value):
         """Setter method for x attribute."""
+        if not isinstance(value, int):
+            raise TypeError('x must be an integer')
+        elif value < 0:
+            raise ValueError('x must be >= 0')
         self.__x = value
 
     @property
@@ -60,4 +72,50 @@ class Rectangle(Base):
     @y.setter
     def y(self, value):
         """Setter method for y attribute."""
+        if not isinstance(value, int):
+            raise TypeError('y must be an integer')
+        elif value < 0:
+            raise ValueError('y must be >= 0')
         self.__y = value
+
+    def area(self):
+        '''
+        Calculate and return the area of the rectangle.
+        '''
+
+        return self.height * self.width
+
+    def display(self):
+        '''
+        Display the Rectangle instance with '#' characters.
+        '''
+
+        for _ in range(self.y):
+            print()
+
+        for _ in range(self.height):
+            print(' ' * self.x + '#' * self.width)
+
+    def __str__(self):
+        '''
+        Override the string representation of the Rectangle.
+        '''
+
+        return "[Rectangle] ({}) {}/{} {}/{}".format(
+                self.id, self.x, self.y, self.width, self.height)
+
+    def update(self, *args, **kwargs):
+        '''
+        Update attributes of the Rectangle.
+        '''
+
+        if len(args) >= 1:
+            self.id = args[0]
+        if len(args) >= 2:
+            self.width = args[1]
+        if len(args) >= 3:
+            self.height = args[2]
+        if len(args) >= 4:
+            self.x = args[3]
+        if len(args) >= 5:
+            self.y = args[4]
