8683600536c08a76f6cd1e6807e7d9280e89c87c
amine-zzr <aminezoukri@gmail.com>
2024-02-11 17:26:51 +0000

    square class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -18,6 +18,17 @@ class Square(Rectangle):
 
         super().__init__(size, size, x, y, id)
 
+    @property
+    def size(self):
+        '''Getter method for size attribute.'''
+        return self.width
+
+    @size.setter
+    def size(self, value):
+        '''Setter method for size attribute.'''
+        self.width = value
+        self.height = value
+
     def __str__(self):
         '''
         Override the string representation of the Square.
@@ -25,3 +36,16 @@ class Square(Rectangle):
 
         return "[Square] ({}) {}/{} - {}".format(
                 self.id, self.x, self.y, self.width)
+
+    def update(self, *args, **kwargs):
+        '''
+        Update attributes of the Square.
+        '''
+
+        if args:
+            attrs = ['id', 'size', 'x', 'y']
+            for i, arg in enumerate(args):
+                setattr(self, attrs[i], arg)
+        else:
+            for key, value in kwargs.items():
+                super().update(**kwargs)
