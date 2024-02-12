c2e47c42ad325807d973d98afb61169a3fa7cc76
amine-zzr <aminezoukri@gmail.com>
2024-02-12 00:00:44 +0000

    tasks till 19

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -61,3 +61,50 @@ class Base:
                 json_str = cls.to_json_string(
                         [obj.to_dictionary() for obj in list_objs])
                 file.write(json_str)
+
+    @staticmethod
+    def from_json_string(json_string):
+        '''
+        Return the list of dictionaries represented by json_string.
+        '''
+
+        if json_string is None or len(json_string) == 0:
+            return []
+        else:
+            return json.loads(json_string)
+
+    @classmethod
+    def create(cls, **dictionary):
+        '''
+        Return an instance with all attributes set based on dictionary.
+        '''
+
+        if cls.__name__ == 'Rectangle':
+            dummy_instance = cls(1, 1)
+        elif cls.__name__ == 'Square':
+            dummy_instance = cls(1)
+        else:
+            raise ValueError('Unsupported class')
+
+        dummy_instance.update(**dictionary)
+        return dummy_instance
+
+    @classmethod
+    def load_from_file(cls):
+        '''
+        Returns a list of instances
+        '''
+
+        filename = cls.__name__ + '.json'
+
+        try:
+            with open(filename, 'r') as file:
+                json_string = file.read()
+                if json_string:
+                    dictionaries = cls.from_json_string(json_string)
+                    instances = [cls.create(**d) for d in dictionaries]
+                    return instances
+                else:
+                    return []
+        except FileNotFoundError:
+            return []
