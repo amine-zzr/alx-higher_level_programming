3761ebeff9e4599dd717de17809fdf4ad93e1742
amine-zzr <aminezoukri@gmail.com>
2024-02-13 01:02:46 +0000

    base unittests

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -11,35 +11,24 @@ class TestBase(unittest.TestCase):
         b = Base()
         self.assertIsInstance(b, Base)
 
-    def test_base_id_increment(self):
-        # Test incrementing ID for each new instance
+    def test_base_id(self):
+        # Test assigning an ID to the Base instance
         b1 = Base()
         b2 = Base()
         self.assertEqual(b1.id, 1)
         self.assertEqual(b2.id, 2)
 
-    def test_base_manual_id(self):
-        # Test creating an instance with a manual ID
-        b = Base(10)
-        self.assertEqual(b.id, 10)
-
-    def test_base_to_json_string_empty_list(self):
-        # Test the to_json_string method with an empty list
+    def test_base_to_json_string(self):
+        # Test the to_json_string method
+        b = Base()
         json_string = Base.to_json_string([])
         self.assertEqual(json_string, "[]")
 
-    def test_base_from_json_string_empty_string(self):
-        # Test the from_json_string method with an empty string
-        obj_list = Base.from_json_string("")
-        self.assertEqual(obj_list, [])
-
-    def test_base_from_json_string_non_empty_string(self):
-        # Test the from_json_string method with a non-empty string
-        json_string = '[{"id": 1}, {"id": 2}]'
+    def test_base_from_json_string(self):
+        # Test the from_json_string method
+        json_string = "[]"
         obj_list = Base.from_json_string(json_string)
-        self.assertEqual(len(obj_list), 2)
-        self.assertEqual(obj_list[0].id, 1)
-        self.assertEqual(obj_list[1].id, 2)
+        self.assertEqual(obj_list, [])
 
 
 if __name__ == '__main__':
