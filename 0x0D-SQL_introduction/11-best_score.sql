ea72900a29905641fb4e240babbf064082be6747
amine-zzr <aminezoukri@gmail.com>
2024-03-14 23:56:29 +0000

    filter records with a score greater than or equal to 10, and then order the results by score in descending order

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- Retrieving reecords from a table specifiying the columns names
+-- and ordering the results in descending order
+SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
