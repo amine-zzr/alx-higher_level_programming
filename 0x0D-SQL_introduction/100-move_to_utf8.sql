504336bb9918f74bb170e4f60a92816d1d3c1186
amine-zzr <aminezoukri@gmail.com>
2024-03-15 02:03:33 +0000

    fixing code

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,5 +1,4 @@
 -- Convert the hbtn_0c_0 database, the first_table table, and the name field
 -- in first_table to UTF-8 (utf8mb4) with the collation utf8mb4_unicode_ci
-ALTER DATABASE hbtn_0c_0 CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
+USE hbtn_0c_0
 ALTER TABLE first_table CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE first_table MODIFY COLUMN name VARCHAR(256) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
