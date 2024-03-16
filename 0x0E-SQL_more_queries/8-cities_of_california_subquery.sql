653c8d112faf0b658f5c0647b6f4c1ee1cd3d8b5
amine-zzr <aminezoukri@gmail.com>
2024-03-16 23:39:51 +0000

    fixing code

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,3 +1,2 @@
 -- Select all cities of California using a subquery
-SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California')
-ORDER BY id ASC;
+SELECT * FROM cities WHERE state_id = (SELECT id FROM states WHERE name = 'California') ORDER BY id ASC;
