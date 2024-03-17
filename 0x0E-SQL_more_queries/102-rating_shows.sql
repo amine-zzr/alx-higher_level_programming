b7db8fccb2b30c5276eea80f6ecc87fc21366768
amine-zzr <aminezoukri@gmail.com>
2024-03-17 03:50:22 +0000

    list all shows from the hbtn_0d_tvshows_rate table by their rating

---
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -0,0 +1,6 @@
+-- List all shows from the "hbtn_0d_tvshows_rate" table by their rating
+SELECT tv_shows.title, SUM(tv_show_ratings.rate) AS rating
+FROM tv_shows
+JOIN tv_show_ratings ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY title
+ORDER BY rating DESC;
