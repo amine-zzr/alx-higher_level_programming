659e2da2305a88ec7014b87f0a249a0efce96a6a
amine-zzr <aminezoukri@gmail.com>
2024-04-10 18:29:00 +0000

    semistandard style

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,12 +2,10 @@
 
 let num = parseInt(process.argv[2], 10);
 
-if (num < 0) {
-  return;
-} else if (!isNaN(num) && num > 0) {
+if (!isNaN(num) && num > 0) {
   while (num--) {
     console.log('C is fun');
   }
-} else {
+} else if (isNaN(num)) {
   console.log('Missing number of occurrences');
 }
