4c814bcdba3cf313a9befe92bc0daa1c20d95438
amine-zzr <aminezoukri@gmail.com>
2024-04-10 18:51:51 +0000

    fixing style

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-let num = parseInt(process.argv[2], 10);
+const num = parseInt(process.argv[2], 10);
 
 if (!isNaN(num) && num > 0) {
   for (let i = 0; i < num; i++) {
@@ -8,7 +8,7 @@ if (!isNaN(num) && num > 0) {
     for (let j = 0; j < num; j++) {
       row += 'X';
     }
-      console.log(row);
+    console.log(row);
   }
 } else if (isNaN(num)) {
   console.log('Missing size');
