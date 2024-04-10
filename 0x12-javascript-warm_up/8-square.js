65ea80fb5ddd8cff83ed277eff3e3c51e55ef338
amine-zzr <aminezoukri@gmail.com>
2024-04-10 18:50:53 +0000

    printing squares

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,15 @@
+#!/usr/bin/node
+
+let num = parseInt(process.argv[2], 10);
+
+if (!isNaN(num) && num > 0) {
+  for (let i = 0; i < num; i++) {
+    let row = '';
+    for (let j = 0; j < num; j++) {
+      row += 'X';
+    }
+      console.log(row);
+  }
+} else if (isNaN(num)) {
+  console.log('Missing size');
+}
