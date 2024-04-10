fdb041e1d920d4bb5f3abddfd75f19e06496b524
amine-zzr <aminezoukri@gmail.com>
2024-04-10 18:17:36 +0000

    fixing a bug

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,7 +2,9 @@
 
 let num = parseInt(process.argv[2], 10);
 
-if (!isNaN(num) && num > 0) {
+if (num < 0) {
+  return;
+} else if (!isNaN(num) && num > 0) {
   while (num--) {
     console.log('C is fun');
   }
