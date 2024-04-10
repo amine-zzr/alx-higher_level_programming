2134374423f08d247ffe356d32f7b81b34977d81
amine-zzr <aminezoukri@gmail.com>
2024-04-10 18:08:07 +0000

    handling thee negative arguments

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -2,10 +2,10 @@
 
 let num = parseInt(process.argv[2], 10);
 
-if (isNaN(num)) {
-  console.log('Missing number of occurrences');
-} else {
+if (!isNaN(num) && num > 0) {
   while (num--) {
     console.log('C is fun');
   }
+} else {
+  console.log('Missing number of occurrences');
 }
