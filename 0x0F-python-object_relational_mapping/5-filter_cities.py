e7fb9ef872067e3f861cd11aadf94b3cb76b18fb
amine-zzr <aminezoukri@gmail.com>
2024-04-16 07:09:20 +0000

    a script that takes in the name of a state as an argument and lists all cities of that state, using the database hbtn_0e_4_usa

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,34 @@
+#!/usr/bin/python3
+
+'''
+    a script that takes in the name of a state as an argument and
+    lists all cities of that state
+    using the database hbtn_0e_4_usa
+'''
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
+    state_name = sys.argv[4]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=mysql_user,
+                         passwd=mysql_password, db=mysql_db)
+    cursor = db.cursor()
+
+    query = """
+            SELECT GROUP_CONCAT(cities.name SEPARATOR ', ')
+            FROM cities
+            JOIN states ON cities.state_id = states.id
+            WHERE states.name = %s
+            ORDER BY cities.id ASC
+            """
+    cursor.execute(query, (state_name,))
+    cities = cursor.fetchall()
+
+    print(cities[0][0])
+
+    db.close()
