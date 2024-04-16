0a7903a38d19da5d75179d4152991f8c67374e2b
amine-zzr <aminezoukri@gmail.com>
2024-04-16 02:02:44 +0000

    a script that lists all states with a name starting with N (upper N) from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+'''a script that lists all states from the database hbtn_0e_0_usa
+    starting with 'N' '''
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    mysql_user = sys.argv[1]
+    mysql_password = sys.argv[2]
+    mysql_db = sys.argv[3]
+
+    db = MySQLdb.connect(host='localhost', port=3306, user=mysql_user,
+                         passwd=mysql_password, db=mysql_db)
+    cursor = db.cursor()
+
+    cursor.execute(
+            "SELECT * FROM states WHERE name LIKE 'N%' ORDER BY id ASC")
+    states = cursor.fetchall()
+
+    for state in states:
+        print(state)
+
+    db.close()
