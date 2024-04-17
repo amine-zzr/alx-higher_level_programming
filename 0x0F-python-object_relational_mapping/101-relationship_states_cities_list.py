be2d325b1e7d153f07bcf5edd6690b8db4751b42
amine-zzr <aminezoukri@gmail.com>
2024-04-17 07:16:31 +0000

    better code

--- 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
+++ 0x0F-python-object_relational_mapping/101-relationship_states_cities_list.py
@@ -10,10 +10,8 @@ from relationship_state import Base, State
 from relationship_city import City
 
 if __name__ == "__main__":
-    username = sys.argv[1]
-    password = sys.argv[2]
-    db_name = sys.argv[3]
 
+    username, password, db_name = sys.argv[1:]
     engine = create_engine(
             f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}")
 
