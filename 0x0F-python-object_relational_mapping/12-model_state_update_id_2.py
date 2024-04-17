f64f9fb05ac0adb010711189cd104f68750ca1f1
amine-zzr <aminezoukri@gmail.com>
2024-04-17 01:33:30 +0000

    a script that changes the name of a State object from the database hbtn_0e_6_usa

---
+++ 0x0F-python-object_relational_mapping/12-model_state_update_id_2.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+"""adding the State object “Louisiana” to a database
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from model_state import Base, State
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+            f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}")
+    Base.metadata.bind = engine
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    state_to_update = session.query(State).filter_by(id=2).first()
+    if state_to_update:
+        state_to_update.name = "New Mexico"
+        session.commit()
+
+    session.close()
