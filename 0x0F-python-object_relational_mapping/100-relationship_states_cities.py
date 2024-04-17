10bf44b0657176f540f27563564a9042761ee6d3
amine-zzr <aminezoukri@gmail.com>
2024-04-17 06:37:27 +0000

    a script that creates the State “California” with the City “San Francisco” from the database hbtn_0e_100_usa

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+"""creating the State “California” with the City
+    “San Francisco” from a database
+"""
+
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from relationship_state import Base, State
+from relationship_city import City
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    db_name = sys.argv[3]
+
+    engine = create_engine(
+            f"mysql+mysqldb://{username}:{password}@localhost:3306/{db_name}")
+    Base.metadata.create_all(engine)
+
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    california = State(name="California")
+    san_francisco = City(name="San Francisco", state=california)
+    session.add(california)
+    session.add(san_francisco)
+    session.commit()
+
+    session.close()
