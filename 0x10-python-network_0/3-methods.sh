5fd4dc245b6378cdbb3da2c0f80d732542aa73a2
amine-zzr <aminezoukri@gmail.com>
2024-04-28 08:35:01 +0000

    fixing code

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Displays all HTTP methods the server will accept
-curl -isX OPTIONS "$1" | grep "Allow:" | cut -d: -f2
+curl -isX OPTIONS "$1" |awk '/Allow:/ {sub("Allow: ", ""); print}'
