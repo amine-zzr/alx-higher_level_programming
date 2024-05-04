b705739502c4b5956edb3f6a7bc3466759e55747
amine-zzr <aminezoukri@gmail.com>
2024-05-04 19:03:05 +0000

    Fetches the status of the ALX intranet

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""
+Fetches the status of the ALX intranet.
+"""
+
+if __name__ == "__main__":
+    import urllib.request
+
+    url = "https://alx-intranet.hbtn.io/status"
+    with urllib.request.urlopen(url) as response:
+        body = response.read()
+        print("Body response:")
+        print(f"    - type: {type(body)}")
+        print(f"    - content: {body}")
+        print(f"    - utf8 content: {body.decode('utf-8')}")
+
