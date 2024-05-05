3c2ad0134f9b4da3c70c2fb40194db43e8d8920c
amine-zzr <aminezoukri@gmail.com>
2024-05-05 13:56:58 +0000

    a Python script that takes in a letter and sends a POST request to http://0.0.0.0:5000/search_user with the letter as a parameter.

---
+++ 0x11-python-network_1/8-json_api.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""
+Sends a POST request with the letter as a parameter.
+Displays the id and name if the response body is properly JSON formatted
+and not empty. Otherwise, displays an error message.
+"""
+
+import requests
+import sys
+import json
+
+if __name__ == "__main__":
+    if len(sys.argv) == 1:
+        q = ""
+    else:
+        q = sys.argv[1]
+    response = requests.post('http://0.0.0.0:5000/search_user', data={'q': q})
+    try:
+        json_response = response.json()
+        if json_response:
+            print("[{}] {}".format(
+                        json_response.get('id'), json_response.get('name')))
+        else:
+            print("No result")
+    except json.JSONDecodeError:
+        print("Not a valid JSON")
