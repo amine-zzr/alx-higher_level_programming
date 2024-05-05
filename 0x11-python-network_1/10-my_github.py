43b870b36aa8ea0de5ba9c687635095d7f929414
amine-zzr <aminezoukri@gmail.com>
2024-05-05 14:51:18 +0000

    Uses Basic Authentication with GitHub API to display user ID.

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""
+Uses Basic Authentication with GitHub API to display user ID.
+"""
+
+import requests
+import sys
+
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    url = "https://api.github.com/user"
+    response = requests.get(url, auth=(username, password))
+
+    if response.status_code == 200:
+        user_id = response.json().get('id')
+        print(user_id)
+    else:
+        print(None)
