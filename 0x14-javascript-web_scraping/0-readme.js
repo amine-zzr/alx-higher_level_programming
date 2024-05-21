f259f4a06d037a14e0ebf283fe7dac89169cc20b
amine-zzr <aminezoukri@gmail.com>
2024-05-21 15:16:23 +0000

    fixing style

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -4,10 +4,10 @@ const fs = require('fs');
 const filePath = process.argv[2];
 
 fs.readFile(filePath, 'utf8', (err, data) => {
-    if (err) {
+  if (err) {
     console.error(err);
     return;
-    }
+  }
 
-    console.log(data);
+  console.log(data);
 });
